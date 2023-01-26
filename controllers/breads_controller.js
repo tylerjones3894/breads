a4a992769f49e85f223d04381f7ad4a18948fda7
Tyler Jones <tylerjones3894@gmail.com>
2023-01-25 20:41:54 -0800

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -32,6 +32,14 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+      bread: Bread[req.params.indexArray],
+      index: req.params.indexArray
+    })
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -49,6 +57,17 @@ breads.delete('/:indexArray', (req, res) => {
     // start, deleteCount
     Bread.splice(req.params.indexArray, 1)
     res.status(303).redirect('/breads')
+  })  
+
+  // UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+    if(req.body.hasGluten === 'on'){
+      req.body.hasGluten = true
+    } else {
+      req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
   })  
 
 module.exports = breads
