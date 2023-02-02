806f84efdff9844379f218797c96fda10d3bb623
Tyler Jones <tylerjones3894@gmail.com>
2023-02-01 20:33:04 -0800

    Code along updates

--- models/bread.js
+++ models/bread.js
@@ -29,7 +29,11 @@ const { Schema } = mongoose
 const breadSchema = new Schema({
     name: { type: String, required: true },
     hasGluten: Boolean,
-    image: { type: String, default: 'https://via.placeholder.com/500' }
+    image: { type: String, default: 'https://via.placeholder.com/500' },
+    baker: {
+        type: String,
+        enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
+    }
 })
 
 // model and export
